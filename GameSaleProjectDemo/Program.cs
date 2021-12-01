8b2ca5e6ee287a7820d177161c1c1eee1a99ea18
CodeLearnn <yusufbjk19_19@hotmail.com>
2021-12-01 14:28:00 +0300

    Project

--- GameSaleProjectDemo/Program.cs
+++ GameSaleProjectDemo/Program.cs
@@ -30,6 +30,8 @@ namespace GameSaleProjectDemo
 
             CampaignManager campaignManager = new CampaignManager();
             campaignManager.Add(campaign1);
+
+            Console.WriteLine();
         }
     }
 }
